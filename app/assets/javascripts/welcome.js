4769d3628c59cddc72bf986c6d3af1bf0bee2d71
Bradley Findeis <bradleyfindeis@gmail.com>
2018-03-06 08:58:45 -0700

    show feature and started the delete feature

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,5 @@
 var arrProducts = [];
-var currentProduct = {};
-var showForm = false;
+
 
 $(document).ready(function() {
   $.ajax({
@@ -10,24 +9,50 @@ $(document).ready(function() {
     arrProducts = products 
     show(arrProducts)
   });
+  $('.submit').on('click', function() {
+
+    var name = this.form.children[0].children[1].value
+    var description = this.form.children[1].children[1].value
+
+    $.ajax({
+      url: `http://json-server.devpointlabs.com/api/v1/products?product[name]=${name}&product[description]=${description}`,
+      method: "POST"
+    }).done( function(product){
+
+    })
+  })
 });
 
-$(document).on('click', '.card', function(){
-  
+// $(document).on("click", ".edit", function() {
+//   alert("Edit")
+// });
+$(document).on("click", ".delete", function() {
   $.ajax({
-    url: `http://json-server.devpointlabs.com/api/v1/products/${this.id}`,
-    method: "GET"
-  }).done( function(product){
-    arrProducts = [product]
-    show(arrProducts); 
-  })
-})
+    url: `http://json-server.devpointlabs.com/api/v1/products?product[name]=${name}&product[description]=${description}`,
+    method: "DELETE"
+  }).done(function(product) {
+    var item = $(`#${id}`)
+    item.parent().parent().remove();
+  });
+});
+
+
+// $(document).on('click', '.card', function(){
+  
+//   $.ajax({
+//     url: `http://json-server.devpointlabs.com/api/v1/products/${this.id}`,
+//     method: "GET"
+//   }).done( function(product){
+//     arrProducts = [product]
+//     show(arrProducts); 
+//   })
+// })
 
 function show(products){
   var productArea = $("#products");
   productArea.empty();
   products.forEach(function(product) {
-    var div = "<div id=" + product.id + ' class="four wide column ui card"><p>' + product.name + "</p><p>" + product.description + '</p><div class="extra content"><div class="ui three buttons"><div class="ui basic blue button">Show</div><div class="ui basic green button">Edit</div><div class="ui basic red button">Delete</div></div></div></div>';
+    var div = "<div id=" + product.id + ' class="four wide column ui card"><p>' + product.name + "</p><p>" + product.description + '</p><div class="extra content"><div class="ui three buttons"><div class="ui basic blue button">Show</div><div class="ui basic green button">Edit</div><div class="ui basic red button delete">Delete</div></div></div></div>';
     productArea.append(div);
   });
 }
